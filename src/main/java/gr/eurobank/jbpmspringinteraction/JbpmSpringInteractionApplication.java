733ff5ea1d6cff77a7184acf330e1e6ec4ccb1ec
Vasileios Bouzas <v-vbouzas@eurobank.gr>
2019-11-14 10:55:54 +0200

    Added configuration for deployment purposes on Application Server

--- src/main/java/gr/eurobank/jbpmspringinteraction/JbpmSpringInteractionApplication.java
+++ src/main/java/gr/eurobank/jbpmspringinteraction/JbpmSpringInteractionApplication.java
@@ -2,9 +2,16 @@ package gr.eurobank.jbpmspringinteraction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JbpmSpringInteractionApplication {
+public class JbpmSpringInteractionApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(JbpmSpringInteractionApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(JbpmSpringInteractionApplication.class, args);
